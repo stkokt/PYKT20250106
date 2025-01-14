d4cafe84e702ae9ac1df6834a73bbc2c79065115
stkokt <stefan.koschnik@mein-karrieretutor.de>
2025-01-14 10:10:27 +0100

    LG2 Aufgaben

---
+++ LG2 Aufgaben.py
@@ -0,0 +1,17 @@
+# 1. Setze das Rindvieh PAP in Code um.
+
+
+
+# 2. Schreibe einen Algorithmus zum Ermitteln,
+#    ob ein Jahr ein Schaltjahr ist.
+
+
+# 3. Ein Auto soll zwei Eigenschaften haben: Farbe und Marke
+#    Ist es ein roter BMW, soll die Ausgabe lauten:
+#    "Das ist ein schönes Auto"
+#    Ist es ein rotes Auto einer anderen Marke, soll die Ausgabe lauten:
+#    "Das ist ein schönes Auto der Marke ..." (die Marke statt der Punkte)
+#    Ist es ein BMW mit einer anderen Farbe, soll die Ausgabe lauten:
+#    z.B. "Das ist ein blauer BMW."
+#    Ist es ein anderes Auto einer anderen Marke, soll die Ausgabe lauten:
+#    z.B. "Das ist ein blaues Auto der Marke ..." (die Marke statt der Punkte)
